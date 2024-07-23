b406ff0b0ed76d9c6d12f587e0f0fdae1fd0f50b
aoisz <an.m284@gmail.com>
2024-07-23 11:23:42 +0700

    Update
    Change Product Preview to be a popup, add PC frame for pc view

--- models/product_preview.py
+++ models/product_preview.py
@@ -7,6 +7,7 @@ class ProductPreview(models.Model):
 
     website_preview_url = fields.Html("Preview", sanitize = False, compute = '_get_html')
     mobile_view = fields.Boolean("Mobile", default=False)
+    show_preview = fields.Boolean("Show Preview", default=False)
 
     def toggle_publish(self):
         for record in self:
@@ -24,15 +25,11 @@ class ProductPreview(models.Model):
         for product in self:
             if(product.id == id):
                 product.mobile_view = not product.mobile_view
-        # return {
-        #     'type': 'ir.actions.client',
-        #     'tag': 'reload'
-        # }
 
     def _get_html(self):
         for product in self:
             url = f"{request.httprequest.host_url}shop/{slug(product)}"
             if(product.mobile_view == True):
-                product.website_preview_url = f'<div class="smartphone"><div class="content"><iframe src="{url}" class="shadow" id="preview_iframe" style="padding-bottom: 180px"><p>Your browser does not support iframes.</p></iframe></div></div>'
+                product.website_preview_url = f'<div class="smartphone_view"><iframe src="{url}" class="shadow" id="preview_iframe"><p>Your browser does not support iframes.</p></iframe></div>'
             else:
-                product.website_preview_url = f'<iframe src="{url}" class="shadow" id="preview_iframe" style="padding-bottom: 58px"><p>Your browser does not support iframes.</p></iframe>'
+                product.website_preview_url = f'<div class="pc_view"><iframe src="{url}" class="shadow" id="preview_iframe" height="24rem"><p>Your browser does not support iframes.</p></iframe><div class="pc_view_power"></div></div>'
